1e733eff5c0f63b669abc801bc0c60c72f89bbf7
Balajivasudevan03 <balajivasudevan.03.11.96@gamil.com>
2025-12-10 16:50:48 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,17 @@
 from flask import Flask
 
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():  
-    return "Hello, World!"
+app = Flask(__name__) # __name__ -> "hello.py"
 
 
-@app.route("/ping", methods=["GET", "post"]) 
+@app.route("/") # endpoint. When user visits "/", this function is automatically     called.
+def hello():
+    return "Hello, there.!"
+
+
+@app.route("/ping", methods=['GET', 'POST']) # only respond to GET and POST requests
 def ping():
-    return {"message" : "why are you pinging me?"} 
+    return {"message": "why are you pinging me?"}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)  # debug=True -> auto-reload server on code changes
